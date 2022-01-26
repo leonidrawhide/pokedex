2ad97906b798ce0151f8a42a500e90f91c5991a6
Leonid (notebook) <leonidrawhide@gmail.com>
2022-01-26 12:52:16 +0300

    made card size flexible | first letter of name is now uppercase

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -4,7 +4,9 @@ export default function PokeCard(props) {
 	const imgLink = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${props.index}.png`
 	const pokemonName = props.item.name.charAt(0).toUpperCase() + props.item.name.slice(1)
   	return <div className='pokecard'>
-		<img src={imgLink} alt={props.item.name} />
+		<div className='img-wrapper'>
+			<img src={imgLink} alt={props.item.name} />
+		</div>
 		<h2>{pokemonName}</h2>
 		{/* <a href={props.item.url}> more </a> */}
   	</div>;
