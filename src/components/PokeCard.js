06697034e10fb6903a71c95abffb33c261f4fa52
Leonid (notebook) <leonidrawhide@gmail.com>
2022-01-26 12:10:03 +0300

    added fixed frame / fixed grids at item's page

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default function PokeCard(props) {
 	const imgLink = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${props.index}.png`
+	const pokemonName = props.item.name.charAt(0).toUpperCase() + props.item.name.slice(1)
   	return <div className='pokecard'>
 		<img src={imgLink} alt={props.item.name} />
-		<h2>{props.item.name}</h2>
+		<h2>{pokemonName}</h2>
 		{/* <a href={props.item.url}> more </a> */}
   	</div>;
 }
