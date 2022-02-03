7730ba328b984c15319dfbdd331eb3c369875945
Leonid (notebook) <leonidrawhide@gmail.com>
2022-02-03 16:17:45 +0300

    navbar is now mobile friendly

--- src/components/PokeNavBar.js
+++ src/components/PokeNavBar.js
@@ -5,14 +5,13 @@ import pokedexLogo from '../img/pokedex-logo.png';
 export default function PokeNavBar() {
   return <div>
     <nav>
-      <div className="nav-wrapper orange darken-4">
-        <Link to="/" className="brand-logo logo-nav">
-          <img src={pokedexLogo} alt="pokedex logo" />  
-        </Link>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><Link to="/">Pokedex</Link></li>
-          <li><Link to="about">About</Link></li>
-        </ul>
+      <div className="poke-nav nav-wrapper orange darken-4">
+          <Link to="/" className="brand-logo logo-nav">
+            <img src={pokedexLogo} alt="pokedex logo" />  
+          </Link>
+          <ul id="nav-mobile" className="right">
+            <li><Link to="about">About</Link></li>
+          </ul>
       </div>
     </nav>
     <Outlet />
